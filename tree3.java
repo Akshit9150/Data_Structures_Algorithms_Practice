c8cef63229f5bcc2b63fc09438b5b02220fc223b
Akshit Verma <146584283+Akshit9150@users.noreply.github.com>
2026-01-07 21:58:46 +0530

    Add files via upload

---
+++ tree3.java
@@ -0,0 +1,30 @@
+//this is for inorder of a tree
+class Node{
+  int data;
+  Node left = null;
+  Node right = null;
+  Node(int data){
+    this.data = data;
+}
+}
+public class tree3{
+  public static void inorder(Node root){
+    if(root == null){
+      return;
+}
+    inorder(root.left);
+    System.out.print(root.data + " ");
+    inorder(root.right);
+}
+  public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+        root.right.left.left = new Node(7);
+        root.right.left.right = new Node(8);
+        inorder(root);
+}
+}
