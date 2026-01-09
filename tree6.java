01815ebddc5c419f4d90390167ae0ee40a139ff7
Akshit Verma <146584283+Akshit9150@users.noreply.github.com>
2026-01-09 22:53:08 +0530

    Add files via upload

---
+++ tree6.java
@@ -0,0 +1,43 @@
+//this code is for left view of the tree
+import java.util.*;
+class Node{
+  int data;
+  Node left = null;
+  Node right = null;
+  Node(int data){
+    this.data= data;
+}
+}
+public class tree6{
+  public static void leftview(Node root, int level, Map<Integer, Integer> map){
+    if(root == null){
+      return;
+}
+    map.putIfAbsent(level, root.data);
+    leftview(root.right, level+1, map);
+    leftview(root.left,level+1, map);
+}
+  public static void leftview(Node root){
+    Map<Integer, Integer> map = new HashMap<>();
+    leftview(root, 1, map);
+    for(int i=1; i<=map.size(); i++){
+     System.out.print(map.get(i)+" ");
+}
+}
+  public static void main(String args[]){
+     Node root = new Node(1);
+     root.left = new Node(2);
+     root.right = new Node(3);
+     root.left.right = new Node(4);
+     root.right.left = new Node(5);
+     root.right.right = new Node(6);
+     root.right.left.left = new Node(7);
+     root.right.left.right = new Node(8);
+     leftview(root);
+}
+}
+
+
+
+
+
